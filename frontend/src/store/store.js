c2cb63051eb35e9e35e918f91a95e657e83843b0
manoj mandal <hackintown99@gmail.com>
2024-07-08 18:03:28 +0530

    auth-update

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,23 @@
-import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import rootReducer from './rootReducer';
-import persistedReducer from './authSlice';
+// store.js
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-const store = createStore(persistedReducer);
+import authSlice from "../features/authSlice"; // Assuming you have a rootReducer
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authSlice);
+
+// Create Redux store with persisted reducer
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// Create a persisted store object
 const persistor = persistStore(store);
 
 export { store, persistor };
