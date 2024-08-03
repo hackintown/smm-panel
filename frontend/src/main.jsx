ffd84574f8be45d18ec15f7656e5e59696ff6415
Manoj Kumar <manojkumarcse432@gmail.com>
2024-08-03 12:59:59 +0530

    vercel added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider>
-          <Router basename="/frontend">
+          <Router>
             <App />
           </Router>
         </ThemeProvider>
