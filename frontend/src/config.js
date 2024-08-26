6a5a0f11022fd775325f69815ce41b55ebaeb16b
manoj mandal <hackintown99@gmail.com>
2024-08-26 18:02:33 +0530

    url-fixed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   apiBaseUrl:
     process.env.NODE_ENV === "production"
-      ? "https://smm-panel-f217.onrender.com/"
+      ? "https://smm-panel-f217.onrender.com"
       : "http://localhost:5000",
 };
 
