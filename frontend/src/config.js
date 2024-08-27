1cba923b4fa35e8ca4c158b25a0c99c58175af46
manoj mandal <hackintown99@gmail.com>
2024-08-27 11:01:37 +0530

    config-file-changed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiBaseUrl: process.env.PUBLIC_API_BASE_URL || "http://localhost:5000",
+  apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000",
 };
 
 export default config;
